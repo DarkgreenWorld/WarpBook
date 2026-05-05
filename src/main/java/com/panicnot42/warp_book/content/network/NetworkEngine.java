7470cdabfbbdf1467cf152f2c59d58ea081db996
深绿世界 <darkgreen_world@outlook.com>
2026-05-06 00:30:44 +0800

    Correct author information

--- src/main/java/com/panicnot42/warp_book/content/network/NetworkEngine.java
+++ src/main/java/com/panicnot42/warp_book/content/network/NetworkEngine.java
@@ -1,12 +1,11 @@
 /**
- * @author ArcAnc, Panicnot42, Darkgreen_World
+ * @author ArcAnc, MelonVRneu, DarkgreenWorld, Panicnot42, FerreusVeritas
  * Created at: 07.08.2024
  * Copyright (c) 2024-2026
  * <p>
  * This code is licensed under "GPL-v3.0-only"
  * Details can be found in the license file in the root folder of this project
  */
-
 package com.panicnot42.warp_book.content.network;
 
 import com.panicnot42.warp_book.Database;
